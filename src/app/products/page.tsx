966c399d8109519ad56e0d8fcd210a3e88649868
Amruta Tandale <Amruta Tandale@TCOGNITION>
2025-02-17 19:06:44 +0530

    Initial Commit

---
+++ src/app/products/page.tsx
@@ -0,0 +1,11 @@
+export default function ProductId() {
+    return (
+        <div>
+            <h1>Products List</h1>
+            <h3>Product 1 </h3>
+            <h3>Product 2 </h3>
+            <h3>Product 3 </h3>
+            <p>Explore our collection of high-quality products.</p>
+        </div>
+    );
+}
